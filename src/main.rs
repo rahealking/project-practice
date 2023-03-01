f97055e5a506f36a2bc2a0070d0a88dcd9c410ec
rahealking <120595889+rahealking@users.noreply.github.com>
2023-03-01 16:00:12 +0000

    array functional information

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use project_practice::*;
-fn main() {
-    let mut message:String=String::new();
-    message=String::from("hello");
-    println!("{}",message);
+// use project_practice::*;
+fn main(){
+    println!("[debug output]");
+    let o=[0,1,2,3,4,5,6];
+    let n=&o[..];
+    let a=&n[..3];
+    let b=&n[3+1..];
+    println!("o:{:?}",o);
+    println!("n:{:?}",n);
+    println!("a:{:?}",a);
+    println!("b:{:?}",b);
+    println!("n.len:{:#?}",n.len());
+    println!("o[2]:({})",o[2]);
+    println!("n[2]:({})",n[2]);
+    println!("a[2]:({})",a[2]);
+    println!("b[2]:({})",b[2]);
+    println!("n[7..]-size:({:?})",&n[7..].len());
+    return;
 }
