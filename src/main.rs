19b90e52b2cec02336c79887a2d897a5af54e142
rahealking <120595889+rahealking@users.noreply.github.com>
2023-07-09 21:53:21 +0000

    added json support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use project_practice::*;
+use serde::{Serializer,Serialize};
+use serde_json::{self,value::RawValue};
 fn main(){
-    let a=backtracing::n_queen(
-        vec![
-            vec!['o','o','o','o'],
-            vec!['o','o','o','o'],
-            vec!['o','o','o','o'],
-            vec!['o','o','o','o']
-        ]
-    );for i in 0..a.len(){
-        println!("{:?}",a[i]);
+    let mut a=vec![
+        vec!['a','b','c','d'],
+        vec!['e','f','g','h'],
+        vec!['i','j','k','l'],
+        vec!['m','n','o','p']
+    ];
+    let b=serde_json::to_string_pretty(&a);
+    match b{
+        Ok(c)=>{
+            println!("{}",c);
+            let mut e:Vec<Vec<char>>=serde_json::from_str(c.as_str()).unwrap();
+            println!("{:?}",e[1]);
+        },Err(d)=>{
+            println!("{}",d);
+        }
     }
-}// implemented n_queen
+}
