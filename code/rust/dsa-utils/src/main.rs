9baee6405603a287e4e76ad8d89379114c20110c
bossagent5010 <rahealking@gmail.com>
2024-10-18 21:11:30 +0530

    proper commit

--- code/rust/dsa-utils/src/main.rs
+++ code/rust/dsa-utils/src/main.rs
@@ -1,5 +1,5 @@
 use dsa_utils as lib;
 fn main(){
     println!("{:?}",lib::bgd::Node::new(1));
-    lib::dat::node();
+    // lib::dat::node();
 }
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-use project_practice as lib;
-fn main(){
-    println!("{:?}",lib::bgd::Node::new(1));
-    lib::dat::node();
-}
